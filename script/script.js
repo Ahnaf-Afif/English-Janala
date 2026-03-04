f20ec8c57d9504fd91c17cd9fd1294e16e6c68b8
Ahnaf-Afif <williamhens123@gmail.com>
2026-03-04 15:50:53 +0600

    section 6 done

--- script/script.js
+++ script/script.js
@@ -16,15 +16,27 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+  if (words.length === 0) {
+    wordContainer.innerHTML = `<div
+                                    class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla"
+                                    >
+                                    <img class = "mx-auto" src = "./assets/alert-error.png" />
+                                    <p class="text-xl font-medium text-gray-600">
+                                        এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+                                    </p>
+                                    <p class="font-bold text-2xl">নেক্সট Lesson এ যান।</p>
+                                </div>`;
+  }
+
   for (let word of words) {
     console.log(word);
     const div = document.createElement("div");
     div.innerHTML = `<div
                         class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4"
                         >
-                        <h2 class="font-bold text-2xl">${word.word}</h2>
+                        <h2 class="font-bold text-2xl">${word.word ? word.word : "No Data Found"} </h2>
                         <p class="font-semibold">Meaning /Pronounciation</p>
-                        <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+                        <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "No Meaning Found"} / ${word.pronunciation ? word.pronunciation : "No Pronunciation Found"}"</div>
                         <div class="flex justify-between items-center">
                             <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]">
                             <i class="fa-solid fa-circle-info"></i>
